f9e4c409bd4444e5acdf574c33e4662f0f11a682
Tatiana <48391271+Mtatiana2402@users.noreply.github.com>
2020-09-30 20:59:41 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
 
 exports.min = function min (array) {
+  if (!array) {
+    return 0;
+  }
+else {
+   var len = array.length, min = Infinity;
+   while (len--) {
+    if (array[len] < min) {
+     min = array[len];
+      }
+    }
+  }
+  if (min == Infinity) {
   return 0;
+  }
+  else return min;
 }
 
 exports.max = function max (array) {
+  if (!array) {
+    return 0;
+  }
+else {
+   var len = array.length, max = -Infinity;
+   while (len--) {
+    if (array[len] > max) {
+     max = array[len];
+      }
+    }
+  }
+  if (max == -Infinity) {
   return 0;
+  }
+  else return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array) {
+    return 0;
+}
+else {
+    var len = array.length;
+    var sum = 0;
+    for (i=0; i<len; i++) {
+        sum +=array[i];
+    }
+}
+if (sum ===0) {
+    return 0;
+}
+else return sum/array.length;
 }
